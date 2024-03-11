0d5e03c5f6561c0fc73ff60f1adf64dd7d86bf7e
callum.f <77156339+TboOffical@users.noreply.github.com>
2024-03-10 22:04:22 -0400

    add plugin for dc

---
+++ Plugins/Contrib/DcPlugin/DcPlugin/PluginImpl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using pGina.Shared.Types;
+
+using log4net;
+
+namespace pGina.Plugin.DcPlugin
+{
+    public class PluginImpl : pGina.Shared.Interfaces.IPluginAuthentication
+    {
+        private ILog m_logger;
+
+        private static readonly Guid m_uuid = new Guid("3F2DBD0E-894C-4335-A983-3C8CA5F3988B");
+
+        public PluginImpl()
+        {
+            m_logger = LogManager.GetLogger("pGina.Plugin.DcPlugin");
+        }
+
+        public string Name
+        {
+            get { return "Hello"; }
+        }
+
+        public string Description
+        {
+            get { return "Authenticates users with DC authentication servers"; }
+        }
+
+        public Guid Uuid
+        {
+            get { return m_uuid; }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public void Starting() { }
+
+        public void Stopping() { }
+
+        public BooleanResult AuthenticateUser(SessionProperties properties)
+        {
+            UserInformation userInfo = properties.GetTrackedSingle<UserInformation>();
+
+            if (userInfo.Username.Contains("hello") && userInfo.Password.Contains("pGina"))
+            {
+                // Successful authentication
+                m_logger.InfoFormat("Successfully authenticated {0}", userInfo.Username);
+                return new BooleanResult() { Success = true };
+            }
+            // Authentication failure
+            m_logger.ErrorFormat("Authentication failed for {0}", userInfo.Username);
+            return new BooleanResult() { Success = false, Message = "Incorrect username or password." };
+        }
+    }
+}
